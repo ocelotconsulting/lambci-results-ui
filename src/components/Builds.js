3414d4d4691f545bb7032faadfc1f16f9f2da2d1
John O'Malley <john.omalley@ocelotconsulting.com>
2016-11-08 14:56:06 -0600

    link to projects

--- src/components/Builds.js
+++ src/components/Builds.js
@@ -1,13 +1,23 @@
 import React, {PropTypes as T} from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router'
 import Spinner from './Spinner'
 import BuildTable from './BuildTable'
+import encode from '../encode'
 
 const Builds = ({builds, params: {bucketId, projectId}}) =>
   builds ? (
     <div className='container builds'>
+      <ol className="breadcrumb">
+        <li>
+          <Link to={`/instances/${encode(bucketId)}`}>Projects</Link>
+        </li>
+        <li className="active">
+          {`${projectId}`}
+        </li>
+      </ol>
       <h3>
-        {`Builds for project ${projectId}`}
+        Builds
       </h3>
       {builds.length ? (
         <BuildTable bucketId={bucketId} projectId={projectId} builds={builds}/>
