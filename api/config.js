91e34c8f98c74b09dae4263115bc340dbb44cb42
Chris Coffman <christopher.coffman@ocelotconsulting.com>
2016-11-11 14:48:16 -0600

    changed stack name by accident

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   region: process.env.AWS_DEFAULT_REGION || 'us-east-1',
   configJsonKey: 'lambci-results-ui-config.json',
-  stackName: process.env.DYNAMO_TABLE_PREFIX || 'Lambci'
+  stackName: process.env.DYNAMO_TABLE_PREFIX || 'lambci'
 }
